69c49507ed1781d14677d42de8507092cc2e6886
Amnish <amnishsingh04@gmail.com>
2023-10-04 23:17:44 -0400

    Add support for TOML config files

--- textml.py
+++ textml.py
@@ -3,8 +3,9 @@
 import os
 import argparse
 import shutil
-
 import convertUtils as utils
+import tomli
+import sys
 
 class Metadata:
     version = "v0.1.7"
@@ -14,7 +15,28 @@ def directory_setup(dir):
         shutil.rmtree(dir)  # Removing directory and its contents
 
     os.mkdir(dir)      # Creating directory
-       
+
+def read_config_file(original_args, file_path):
+    """Parses the toml configuration file, returns the parsed options as a dictionary"""
+
+    # Start with default arguments
+    args = original_args
+
+    # Read the TOML file
+    try:
+        with open(file_path, "rb") as file:
+                custom_configuration = tomli.load(file)
+    except tomli.TOMLDecodeError:
+        sys.exit("Supplied TOML file is not valid.")
+    except:
+        sys.exit("File path supplied to --config or -c option is invalid.")
+
+    # Set the specified options, use default if any not specified 
+    for (key, value) in custom_configuration.items():
+        setattr(args, key, value)
+
+    return args
+
 def main():
     outDir = "til/"              # Setting default output directory  
     lang = "en-CA"
@@ -26,8 +48,12 @@ def main():
     parser.add_argument('input', metavar='input', type=str, help="The provided .txt/.md file or a folder/directory of .txt/.md files to be converted")
     parser.add_argument('-o','--output', metavar='output', type=str, help="Optionally specifies a directory to save converted HTML files (Creates folder if one does not exist")
     parser.add_argument('-l','--lang', metavar='lang', type=str, help="Specifies a language for the <html lang=...> element in the <html> root element")
+    parser.add_argument('-c','--config', metavar='config', type=str, help="Allows to specify all the options in a TOML formatted configuration file instead of having to pass them all as command line arguments every time.")
 
     args = parser.parse_args()
+
+    if args.config is not None:
+        args = read_config_file(args, args.config)
 
     if args.output:
         outDir = args.output
